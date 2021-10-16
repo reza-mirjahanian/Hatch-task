7282ee9add5a7e44827ea0715b9784b5a3cc1713
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-10-16 18:55:01 +0330

    test

--- test/respository/Post.spec.js
+++ test/respository/Post.spec.js
@@ -12,98 +12,115 @@ const mockPostsDesign = require('../mockData/blog-api/posts_design.json'); //htt
 
 
 suite('Testing Post Repository', () => {
-    suite('async fetchPost(tag)', () => {
-        test('should return post for the "tech" tag correctly', async () => {
-            const postRepo = new PostRepo();
-            const tag = 'tech';
-            nock(postRepo.HOST)
-                .get(postRepo.PATH)
-                .query(new URLSearchParams(`tag=${tag}`))
-                .reply(200, mockPostsTech); // Not found!.
-            const result = await postRepo.fetchPost(tag);
-            expect(result).to.be.an('array').that.have.lengthOf(28);
-            expect(_.keys(result[0])).to.be.deep.equal(['author', 'authorId', 'id', 'likes', 'popularity', 'reads', 'tags'])
-            expect(result[0].tags).to.be.deep.equal(['tech', 'health'])
+  suite('async fetchDataService(tag)', () => {
+    test('should return post for the "tech" tag correctly', async () => {
+      const postRepo = new PostRepo();
+      const tag = 'tech';
+      nock(postRepo.HOST)
+        .get(postRepo.PATH)
+        .query(new URLSearchParams(`tag=${tag}`))
+        .reply(200, mockPostsTech);
+      const result = await postRepo.fetchDataService(tag);
+      expect(result).to.be.an('array').that.have.lengthOf(28);
+      expect(_.keys(result[0])).to.be.deep.equal(['author', 'authorId', 'id', 'likes', 'popularity', 'reads', 'tags'])
+      expect(result[0].tags).to.be.deep.equal(['tech', 'health'])
 
-            nock.cleanAll();
-        });
-
-        test('should return Error for the wrong tag', async () => {
-            const postRepo = new PostRepo();
-            try {
-                await postRepo.fetchPost('')
-                expect(1).to.be.equal(2);
-            } catch (e) {
-                expect(e.message).to.be.equal('Post:fetchPost()')
-            }
-        });
+      nock.cleanAll();
     });
 
-    suite('async getPosts()', () => {
-        test('should return Error for the wrong tag parameter', async () => {
-            const postRepo = new PostRepo();
-            try {
-                await postRepo.getPosts('', {})
-                expect(1).to.be.equal(2);
-            } catch (e) {
-                expect(e.message).to.be.equal('Tags parameter is required')
-            }
-            try {
-                await postRepo.getPosts([], {})
-                expect(1).to.be.equal(2);
-            } catch (e) {
-                expect(e.message).to.be.equal('Tags parameter is required')
-            }
-        });
+    test('should return Error for the wrong tag', async () => {
+      const postRepo = new PostRepo();
+      try {
+        await postRepo.fetchDataService('')
+        expect(1).to.be.equal(2);
+      } catch (e) {
+        expect(e.message).to.be.equal('Post:fetchDataService()')
+      }
+    });
+  });
 
-        test('should return Error for the wrong sort parameters', async () => {
-            const postRepo = new PostRepo();
-            try {
-                await postRepo.getPosts(['tech'], {
-                    direction: 'left'
-                })
-                expect(1).to.be.equal(2);
-            } catch (e) {
-                expect(e.message).to.be.equal('sortBy parameter is invalid')
-            }
-
-            try {
-                await postRepo.getPosts(['tech', 'health'], {
-                    sortBy: 'price'
-                })
-                expect(1).to.be.equal(2);
-            } catch (e) {
-                expect(e.message).to.be.equal('sortBy parameter is invalid')
-            }
-
-            try {
-                await postRepo.getPosts(['tech', 'health'], {
-                    sortBy: 35
-                })
-                expect(1).to.be.equal(2);
-            } catch (e) {
-                expect(e.message).to.be.equal('sortBy parameter is invalid')
-            }
-
-        });
+  suite('async getPosts()', () => {
+    test('should return Error for the wrong tag parameter', async () => {
+      const postRepo = new PostRepo();
+      try {
+        await postRepo.getPosts('')
+        expect(1).to.be.equal(2);
+      } catch (e) {
+        expect(e.message).to.be.equal('Tags parameter is required')
+      }
+      try {
+        await postRepo.getPosts([])
+        expect(1).to.be.equal(2);
+      } catch (e) {
+        expect(e.message).to.be.equal('Tags parameter is required')
+      }
     });
 
-    test.only('should return post for the "tech,history" tags correctly', async () => {
-        const postRepo = new PostRepo();
-        const techTag = 'tech';
-        const historyTag = 'history';
-        const tags = [techTag,historyTag];
-        nock(postRepo.HOST)
-            .get(postRepo.PATH)
-            .query(new URLSearchParams(`tag=${techTag}`))
-            .reply(200, mockPostsTech); // Not found!.
-        const result = await postRepo.getPosts(tags,{});
-        // expect(result).to.be.an('array').that.have.lengthOf(28);
-        // expect(_.keys(result[0])).to.be.deep.equal(['author', 'authorId', 'id', 'likes', 'popularity', 'reads', 'tags'])
-        // expect(result[0].tags).to.be.deep.equal(['tech', 'health'])
+    test('should return post for the "tech,history" tags correctly', async () => {
+      const postRepo = new PostRepo();
+      const techTag = 'tech';
+      const historyTag = 'history';
+      const tags = [techTag, historyTag];
+      nock(postRepo.HOST)
+        .get(postRepo.PATH)
+        .query(new URLSearchParams(`tag=${techTag}`))
+        .reply(200, mockPostsTech);
 
-        nock.cleanAll();
+      nock(postRepo.HOST)
+        .get(postRepo.PATH)
+        .query(new URLSearchParams(`tag=${historyTag}`))
+        .reply(200, mockPostsHistory);
+      const result = await postRepo.getPosts(tags);
+      // expect(result).to.be.an('array').that.have.lengthOf(28);
+      // expect(_.keys(result[0])).to.be.deep.equal(['author', 'authorId', 'id', 'likes', 'popularity', 'reads', 'tags'])
+      // expect(result[0].tags).to.be.deep.equal(['tech', 'health'])
+
+      nock.cleanAll();
     });
+  });
+
+
+
+  suite('mergePost()', () => {
+
+    test('should return Error for the wrong sort parameters', async () => {
+      const postRepo = new PostRepo();
+      try {
+        await postRepo.mergePost([], {
+          direction: 'left'
+        })
+        expect(1).to.be.equal(2);
+      } catch (e) {
+        expect(e.message).to.be.equal('sortBy parameter is invalid')
+      }
+
+      try {
+        await postRepo.mergePost([], {
+          sortBy: 'price'
+        })
+        expect(1).to.be.equal(2);
+      } catch (e) {
+        expect(e.message).to.be.equal('sortBy parameter is invalid')
+      }
+
+      try {
+        await postRepo.mergePost([], {
+          sortBy: 35
+        })
+        expect(1).to.be.equal(2);
+      } catch (e) {
+        expect(e.message).to.be.equal('sortBy parameter is invalid')
+      }
+
+        // const result = await postRepo.getPosts(tags, {
+        //     sortBy: 'likes',
+        //     direction: 'desc'
+        // });
+
+    });
+
+
+  });
 
 
 });
